0c4b331d75954ee3f4ad8ef9fdf08f456b0121cb
Luís Ferreira <contact@lsferreira.net>
2019-05-06 13:33:02 +0100

    package: change Main structure
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,15 @@ import net.lsferreira.school.trabalholaboratorial.aulas.gestor.GestorSemanaAulas
 
 public class Main {
 
-    public Main() {
-        System.out.println();
-        for (Aula aula : GestorSemanaAulas.INSTANCIA.getAulas()) {
-            System.out.println(aula);
-            System.out.println("\n----------------------------------------\n");
-        }
-    }
+	public Main() {
+		System.out.println();
+		for (Aula aula : GestorSemanaAulas.INSTANCIA.getAulas()) {
+			System.out.println(aula);
+			System.out.println("\n----------------------------------------\n");
+		}
+	}
 
-    public static void main(String[] args) {
-        new Main();
-    }
+	public static void main(String[] args) {
+		new Main();
+	}
 }
